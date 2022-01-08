29bc557e3892376687ce6a8cb8e15a391db38837
amuslera <arielmuslera@gmail.com>
2022-01-07 18:12:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ejercicio_individual_express
+ 
---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express();
+const path = require ('path');
+app.listen(3000, () => console.log("Servidor corriendo"));
+
+/*app.get('/', function(req,res){
+    res.send('Bienvenidos al sitio');
+});
+
+*/
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/views/home.html');
+})
+
+app.use(express.static(path.join(__dirname, './public')));
+
+
+
